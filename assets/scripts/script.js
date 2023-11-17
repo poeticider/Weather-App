398eed546077e5ba40065afa870c4d83c6931123
Orange Workbook <d.riley0189@gmail.com>
2023-11-17 19:49:40 +0000

    adds event listener that targets search button. Removes jquery

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+const findWeather = (e) => {
+    e.preventDefault();
+    
+    console.log("hello")
+
+
+   
+}
+
+let buttonEl = document.getElementById('search-button');
+buttonEl.addEventListener('click', findWeather);
